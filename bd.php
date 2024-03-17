16b58f2b19571d5be056e635b6481d5af8f9c2a2
Elvin Eduardo <franco193@hotmail.com>
2024-03-16 23:56:13 -0400

    se le agregó estructura al index y se crearon las propiedades bases dentro de la pagina bd.php

--- bd.php
+++ bd.php
@@ -1,5 +1,15 @@
 <?php
 
+$servidor="localhost";
+$baseDeDatos="app";
+$usuario="root";
+$contraseña="";
+
+try{
+    $conexion= new PDO("mysql:host=$servidor;dbname=$baseDeDatos",$usuario,$contraseña);
+}catch(Exception $ex){
+    echo $ex->getMessage();
+}
 
 
 ?>
