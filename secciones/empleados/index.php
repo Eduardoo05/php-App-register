42921964820e614399d57b29a4d754d0d7fa2ec4
Elvin Eduardo <franco193@hotmail.com>
2024-03-18 23:58:03 -0400

    se crearon los botones editables sin logica para la seccion de empleados

--- secciones/empleados/index.php
+++ secciones/empleados/index.php
@@ -1,5 +1,46 @@
 <?php include("../../templates/header.php"); ?>
 
-Listar empleados
+</br>
+
+<div class="card">
+    <div class="card-header">Empleados</div>
+    <div class="card-body">
+        
+
+    <div
+        class="table-responsive-sm"
+    >
+        <table
+            class="table"
+        >
+            <thead>
+                <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Foto</th>
+                    <th scope="col">CV</th>
+                    <th scope="col">Puesto</th>
+                    <th scope="col">Fecha de ingreso</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="">
+                    <td scope="row">Eduardo Franco</td>
+                    <td>imagen.jpg</td>
+                    <td>CV.pdf</td>
+                    <td>Programador Jr.</td>
+                    <td>18/03/2024</td>
+                    <td>Carta|Editar|Eliminar</td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
+    
+
+    </div>
+    
+</div>
+
 
 <?php include("../../templates/footer.php"); ?>
