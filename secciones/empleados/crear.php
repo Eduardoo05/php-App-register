e88ed4fb9f2a6c5d0002e897730e6a64a7c24132
Elvin Eduardo <franco193@hotmail.com>
2024-03-23 23:50:31 -0400

    creacion de formularios en la carpeta empleados, hoja crear.php

--- secciones/empleados/crear.php
+++ secciones/empleados/crear.php
@@ -1,5 +1,27 @@
 <?php include("../../templates/header.php"); ?>
+<br/>
+<div class="card">
+    <div class="card-header">Datos del empleado</div>
+    <div class="card-body">
+        
+    <form action="" method="post" enctype="multipart/form-data"></form>
 
-Crear empleados
+    <div class="mb-3">
+        <label for="primernombre" class="form-label">Primer nombre</label>
+        <input
+            type="text"
+            class="form-control"
+            name="primernombre"
+            id="primernombre"
+            aria-describedby="helpId"
+            placeholder="Primer nombre"
+        />
+        
+    </div>
+    
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
+
 
 <?php include("../../templates/footer.php"); ?>
