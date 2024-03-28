c1d1908df4f086942494f37afe08ae1ac45846ef
Elvin Eduardo <franco193@hotmail.com>
2024-03-28 01:22:51 -0400

    se creó el metodo get y post para la edicion de los usuarios

--- secciones/usuarios/editar.php
+++ secciones/usuarios/editar.php
@@ -6,13 +6,44 @@
 
     if(isset($_GET['editText'])){
         //vamos a almacenar el id
-        $editarText=(isset($_GET['$editarText'])?$_GET['editarText']:"");
+        $txtID=(isset($_GET['$txtID'])?$_GET['txtID']:"");
         //preparamos una instruccion SQL
         $sentencia=$conexion->prepare("SELECT * FROM tbl_usuarios WHERE id=:id");
         //pasamos un parametro para editarlo
-        $sentencia->bindParam("editarText", $editarText);
+        $sentencia->bindParam("txtID", $txtID);
         //se ejecuta la posibilidad de edicion
         $sentencia->execute();
+        $registro=$sentencia->fetch(PDO::FETCH_LAZY);
+        $usuario=$registro['usuario'];
+        $passwordText=$registro['password'];
+        $correo=$registro['correo'];
+        //validar compatibilidad entre variables y datos
+
+
+    }
+
+    if($_POST){
+
+        //recolectamos los datos del metodo post
+        $txtID=(isset($_POST['txtID'])?$_POST['txtID']:"");
+        //conectamos los inputs con los conectores de la base de datos
+        $usuario=(isset($_POST['editarNombre'])?$_POST['editarNombre']:"");
+        $passwordText=(isset($_POST['editarPassword'])?$_POST['editarPassword']:"");
+        $correo=(isset($_POST['editarCorreo'])?$_POST['editarCorreo']:"");
+        //se prepara la insercion de datos mediante una sentencia
+        $sentencia=$conexion->prepare("UPDATE tbl_usuarios SET usuarios=:editarNombre 
+            /*password=:editarPassword correo=:editarCorreo*/ WHERE id=:id");
+        $sentencia=$conexion->prepare("UPDATE tbl_usuarios SET passwordText=:editarPassword WHERE id=:id");
+        //asignando los valores que vienen de la base de datos y el metodo post con bindParam
+        $sentencia->bindParam("usuario", $usuario);
+        $sentencia->bindParam("password", $passwordText);
+        $sentencia->bindParam("correo", $correo);
+        //volvemos el metodo global txtID en que vaya vinculado al id que caiga asignado
+        $sentencia->bindParam(":id", $txtID);
+        //ejecutar el post
+        $sentencia->execute();
+        //redirigir una vez ejecutado
+        header("Location:index.php");
     }
 
 ?>
@@ -25,15 +56,15 @@
         <form action="" method="post" enctype="multipart/form-data">
 
             <div class="mb-3">
-                <label for="" class="form-label">ID</label>
+                <label for="txtID" class="form-label">ID</label>
                 <input
                     type="text"
                     class="form-control"
-                    name=""
-                    id=""
+                    name="txtID"
+                    id="txtID"
                     aria-describedby="helpId"
                     placeholder="ID"
-                    value=""
+                    value="<?php echo $txtID; ?>"
                     readonly
                 />
                 
@@ -48,7 +79,7 @@
                     id="editarNombre"
                     aria-describedby="helpId"
                     placeholder="Escribe nuevo usuario"
-                    value=""
+                    value="<?php echo $usuario; ?>"
                 />
                 
             </div>
@@ -62,7 +93,7 @@
                     id="editarPassword"
                     aria-describedby="helpId"
                     placeholder="Escribe nueva contraseña"
-                    value=""
+                    value="<?php echo $passwordText; ?>"
                 />
                 
             </div>
@@ -76,7 +107,7 @@
                     id="editarCorreo"
                     aria-describedby="helpId"
                     placeholder="Escribe nuevo correo"
-                    value=""
+                    value="<?php echo $correo; ?>"
                 />
                 
             </div>
